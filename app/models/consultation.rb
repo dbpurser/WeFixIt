ea068b7b3a8afef422e164568f717b348994f328
Joshua Deason <jadeason@memphis.edu>
2021-04-17 17:08:21 -0500

    Added Validatons to descrition and date for the Consultation model

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -15,5 +15,12 @@ class Consultation < ApplicationRecord
     # belongs_to :user, through: :device 
 
     validates :description, presence: true, allow_blank: false
-    
+    validates :date, presence: true, allow_nil: false
+    validate :date_not_in_past
+    def date_not_in_past
+        return if date.nil?
+        return if Date.today <= date
+        errors.add(:date, "Error! Incorrect Date Chosen.")
+
+    end
 end
