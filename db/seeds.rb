bd57f5f070159d71f3c68a78d4f46e317f5cc57e
Joshua Deason <jadeason@memphis.edu>
2021-04-17 16:41:45 -0500

    Added Consultation Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#scaffold for creating a date
+#Consultation.create(description:"", date:"2021-01-01".to_date)
+
+#dummy data
+Consultation.create(description:"Screen Repair Consultation", date:"2021-01-03".to_date)
+
+Consultation.create(description:"Battery Replacement Consultation", date:"2021-02-01".to_date)
