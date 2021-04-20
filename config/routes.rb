4d854e161842fc883f894ae8fca8c0945cdfe712
Dustin Purser <dpurser@memphis.edu>
2021-04-19 22:59:45 -0500

    Iss 24, added user profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: redirect('/home')
+  
 
   get 'home', to: 'pages#home', as: 'home'
+  get 'user/:id', to: 'user#show', as: 'user'
+  
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2021_04_19_051047) do
 
   # These are extensions that must be enabled in order to support this database
