3beed454cf05371e45946c96f586039b2a9d8184
Geo Garcia <garciageovanni10@yahoo.com>
2021-04-26 00:50:57 -0500

    add Receiver model class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_065804) do
+ActiveRecord::Schema.define(version: 2021_05_06_065805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 2021_05_06_065804) do
 
   create_table "notifications", force: :cascade do |t|
     t.string "notif_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "receivers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
