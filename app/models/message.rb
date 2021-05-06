7fd0893af0bab59b7423a07b0740796d74af72c9
Josiah Anderson <riddlerforever@live.com>
2021-05-06 04:57:05 -0500

    added a few methods to message model class to retrieve the sender and reciever respective names, emails and usernames

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,4 +8,31 @@
 #  updated_at :datetime         not null
 #
 class Message < ApplicationRecord
+
+
+
+
+    def sender_name
+        sender.first_name
+    end
+
+    def reciever_name
+        reciever.first_name
+    end
+
+    def sender_email
+        sender.email
+    end
+
+    def reciever_email
+        reciever.email
+    end
+
+    def sender_user_name
+        sender.grab_user_name
+    end
+
+    def reciever_user_name
+        reciever.grab_user_name
+    end
 end
