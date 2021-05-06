76bdcbc0ffef70b5017befa7d661e02e8ea1bf5a
Josiah Anderson <riddlerforever@live.com>
2021-05-06 16:54:31 -0500

    Added the final methods to message class that invoke the methods in reciever and sender to retrieve the user information

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,5 +8,27 @@
 #  updated_at :datetime         not null
 #
 class Message < ApplicationRecord
-    
+    def sender_name
+        sender.first_name
+    end
+
+    def reciever_name
+        reciever.first_name
+    end
+
+    def sender_email
+        sender.email
+    end
+
+    def reciever_email
+        reciever.email
+    end
+
+    def sender_user_name
+        sender.user_name
+    end
+
+    def reciever_user_name
+        reciever.user_name
+    end
 end
