b5443a52bb2853054a9a5ce73bdbe2a36c5ac2e6
Joshua Deason <jadeason@memphis.edu>
2021-05-06 18:15:58 -0500

    Add Model Test Validations to Consultation Class

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2021_04_19_051048) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "consultations", force: :cascade do |t|
     t.text "description"
     t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
-  
-   create_table "devices", force: :cascade do |t|
+
+  create_table "devices", force: :cascade do |t|
     t.string "deviceType"
     t.string "model"
     t.string "brand"
