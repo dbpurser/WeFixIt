6fe0fcbf1013738ed81d17e5df04b27c1fff9a17
Josiah Anderson <riddlerforever@live.com>
2021-05-07 06:31:01 -0500

    Added render :show to the controller action

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,16 +1,13 @@
 class ConsultationsController < ApplicationController
-<<<<<<< HEAD
-    
-    def index
-    end
 
-    def show
-        @consultation = Consultation.find_by_id(params[:id])
-    end
 
-=======
+  def show
+    @consultation = Consultation.find_by_id(params[:id])
+    render :show
+  end
+
   def index
     render :index
   end
->>>>>>> main
+
 end
