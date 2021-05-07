fff25cd90be2ff1ea311b7588c41c456e5c8ca32
Josiah Anderson <riddlerforever@live.com>
2021-05-06 22:50:29 -0500

    I added a inbox/outbox index page. And a little bit of stlye as well

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
     def index
         @inbox=true
         @user = User.find(params[:user_id])
-        if @user.reciever.nil?
+        if @user.receiver.nil?
             @messages_recieved = "no messages recieved"
         else
             @messages_recieved = @user.reciever.messages
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
     def outbox
         @inbox=false
         @user = User.find(params[:user_id])
-        if @user.reciever.nil?
+        if @user.receiver.nil?
             @messages_recieved = "no messages recieved"
         else
             @messages_recieved = @user.reciever.messages
